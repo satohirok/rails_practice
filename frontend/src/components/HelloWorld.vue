c43a894d726141d683c7a243d7b16d17e335e45b
hi-sato <hi-sato@ceres-inc.jp>
2026-06-10 14:56:24 +0900

    first commit

---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { ref } from 'vue'
+const count = ref(0)
+</script>
+
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <button @click="count++">Count is: {{ count }}</button>
+  </div>
+</template>
